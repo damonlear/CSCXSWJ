1eed9f757a21b8124282427f5c1e4c4c5926a43b
damon_lee <damon_lee@aliyun.com>
2020-07-23 18:09:15 +0800

    new

--- Form1.cs
+++ Form1.cs
@@ -18,8 +18,6 @@ namespace WindowsFormsApp3
     {
         private IPAddress serverIP = IPAddress.Parse("127.0.0.1");
         private int serverPort = 8375;
-        private IPEndPoint serverFullAddr;
-        private Socket sock;
 
         public Form1()
         {
@@ -28,24 +26,27 @@ namespace WindowsFormsApp3
 
         private void Form1_Load(object sender, EventArgs e)
         {
-      
             InitEvent();
         }
 
         private void InitEvent()
         {
+            checkADBEnvironment();
+
             setButtonEnable(0);
             timer_total.Start();
-            checkADBEnvironment();
+
+            Thread thrListener = new Thread(new ThreadStart(checkADBDeviceList));
+            thrListener.Start();
         }
-        
+
         private void checkADBEnvironment()
         {
             if (cmdShell("adb version").Length <= 0)
             {
-                DialogResult dialogResult = MessageBox.Show("系统缺少ADB运行环境，无法运行","警告");
+                DialogResult dialogResult = MessageBox.Show("系统缺少ADB运行环境，无法运行", "警告");
                 if (dialogResult == DialogResult.OK || dialogResult == DialogResult.Yes)
-                { 
+                {
                     System.Environment.Exit(0);
                 }
             }
@@ -55,29 +56,70 @@ namespace WindowsFormsApp3
         {
             while (true)
             {
-                if (cmdShell("adb version").Length < 0 )
+                string online = cmdShell("adb shell getprop ro.product.model");
+                Console.WriteLine(online);
+                Console.WriteLine(online.Contains("device not found"));
+                if (online.Length <= 0 || online.Contains("device not found"))
                 {
-
+                    this.Invoke(new EventHandler(delegate {
+                        text_status.Text = "设备未连接USB";
+                    }));
+                    Thread.Sleep(1000);
+                    continue;
                 }
+                else
+                {
+                    this.Invoke(new EventHandler(delegate {
+                        text_status.Text = online;
+                    }));
+                }
+                string isForward = cmdShell("adb forward --list");
+                if (isForward.Length <= 0 || isForward.Contains("device not found"))
+                {
+                    this.Invoke(new EventHandler(delegate {
+                        text_status.Text = "USB已连接，转发通道未连接";
+                    }));
+                    Thread.Sleep(1000);
+                    continue;
+                }
+                if(tcpClient == null || !tcpClient.Connected)
+                {
+                    this.Invoke(new EventHandler(delegate {
+                        text_status.Text = "USB已连接，socket未连接";
+                    }));
+                    Thread.Sleep(5000);
+                }
+                else
+                {
+                    this.Invoke(new EventHandler(delegate {
+                        text_status.Text = "USB已连接，socket已连接";
+                    }));
+                    Thread.Sleep(10000);
+                }
+
             }
         }
 
         //-1false 0 null 1download 2unload 3select
         private void setButtonEnable(int status)
         {
-            if(status < 0)
+            if (status < 0)
             {
                 button_select.Enabled = false;
                 button_download.Enabled = false;
                 button_upload.Enabled = false;
+                button_connect_device.Enabled = false;
+                button_disconnect_device.Enabled = false;
             }
             else if (status == 0)
             {
                 button_select.Enabled = true;
                 button_download.Enabled = true;
                 button_upload.Enabled = true;
+                button_connect_device.Enabled = true;
+                button_disconnect_device.Enabled = true;
             }
-            else if(status == 1)
+            else if (status == 1)
             {
                 button_select.Enabled = false;
                 button_download.Enabled = true;
@@ -166,7 +208,7 @@ namespace WindowsFormsApp3
 
         private void button_download_Click(object sender, EventArgs e)
         {
-            
+
         }
         private void button_upload_Click(object sender, EventArgs e)
         {
@@ -175,7 +217,8 @@ namespace WindowsFormsApp3
         //IP输入
         private void edittext_ip_TextChanged(object sender, EventArgs e)
         {
-            if (IPAddress.TryParse(edittext_ip.Text,out serverIP)) {
+            if (IPAddress.TryParse(edittext_ip.Text, out serverIP))
+            {
                 serverIP = IPAddress.Parse(edittext_ip.Text);
                 text_ip_regex.Text = "";
             }
@@ -201,22 +244,71 @@ namespace WindowsFormsApp3
             }
         }
 
-        private void button_connect_device_Click(object sender, EventArgs e)
-        {   
+        private void adbForward()
+        {
+            this.Invoke(new EventHandler(delegate
+            {
+                setButtonEnable(-1);
+            }));
             cmdShell("adb kill-server");
             cmdShell("adb devices");
             cmdShell($"adb forward tcp:{serverPort} tcp:{serverPort}");
             string s = cmdShell("adb forward --list");
             if (s.Contains("device not found") || s.Trim().Length <= 0)
             {
-                text_status.Text = "ADB-TCP通道建立失败或设备未连接";
+                text_status.Invoke(new EventHandler(delegate
+                {
+                    text_status.Text = "ADB-TCP通道建立失败或设备未连接";
+                }));
             }
+            this.Invoke(new EventHandler(delegate
+            {
+                setButtonEnable(0);
+            }));
             // TODO connet device
+        }
+        private void button_connect_device_Click(object sender, EventArgs e)
+        {
+            Thread thrListener = new Thread(new ThreadStart(adbForward));
+            thrListener.Start();
+
         }
 
         private void button_disconnect_device_Click(object sender, EventArgs e)
         {
-            cmdShell($"adb forward --remove tcp:{serverPort}");
+            //   cmdShell($"adb forward --remove tcp:{serverPort}");
+            Thread tcpServerListener = new Thread(new ThreadStart(TcpClient));
+            tcpServerListener.Start();
+        }
+
+        //安卓设备作为服务端
+        //pc作为客户端
+        private TcpClient tcpClient;
+        private void TcpClient()
+        {
+            tcpClient = new TcpClient();
+            tcpClient.Connect(serverIP, serverPort);
+
+            NetworkStream ntwStream = tcpClient.GetStream();
+            if (ntwStream.CanWrite)
+            {
+                //    Byte[] bytSend = Encoding.UTF8.GetBytes(txtSendMssg.Text);
+                Byte[] bytSend = new byte[] { 0xE9 , 0x01 , 0x02 , 0x03 , 0x04 };
+                ntwStream.Write(bytSend, 0, bytSend.Length);
+            }
+            else
+            {
+                MessageBox.Show("无法写入数据流");
+
+                ntwStream.Close();
+                tcpClient.Close();
+
+                return;
+            }
+
+            ntwStream.Close();
+            tcpClient.Close();
         }
     }
 }
+
